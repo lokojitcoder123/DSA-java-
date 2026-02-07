1307d9521efd70e4447bac38f24d00e39f4e3468
Lokojit Biswas <lokojitb@gmail.com>
2026-02-08 00:33:51 +0530

    Time: 241 ms (5.03%) | Memory: 47.9 MB (33.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+       int n = arr.length; 
+       for(int i = 0;i<n-1;i++){ 
+           for(int j=0;j<n-1-i; j++){
+            if(arr[j]==0){
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]= temp;
+            }
+           }
+       }
+    }
+}
